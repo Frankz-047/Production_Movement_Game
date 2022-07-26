50f60058711985db05c0868ee2a3e4973e90095e
Frankz-047 <Frankz4816@gmail.com>
2022-07-25 23:57:38 -0700

    Remove duplicated wallrun.cs script
    - Remove duplicated wallrun.cs script
    - Move all existing script into Scripts folder

--- Movement_Game_Project/Assets/Scripts/WallRun.cs
+++ Movement_Game_Project/Assets/Scripts/WallRun.cs
@@ -7,15 +7,15 @@ public class WallRun : MonoBehaviour
     [Header("Wall Running Setting")]
     public LayerMask isWall;
     public LayerMask isGround;
-    public float wallRunForce, maxWallrunTime, maxWallSpeed;
+    public float wallRunForce, maxWallrunTime, wallrunTime;
 
     [Header("Input")]
     private float horizontalInput, verticalInput;
 
     [Header("Detection")]
     public float wallCheckDistance, minJunpHeight;
-    private bool isWallRight, isWallLeft;
-    private RaycastHit leftWall, rightWall;
+    private bool wallRight, wallLeft;
+    private RaycastHit leftWallHit, rightWallHit;
 
     [Header("Reference")]
     public Transform orientation;
@@ -24,5 +24,64 @@ public class WallRun : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        CheckWall();
+    }
+
+    //private void FixedUpdate()
+    //{
+    //}
+    
+    private void CheckWall()
+    {
+        wallRight = Physics.Raycast(transform.position, orientation.right, out rightWallHit, wallCheckDistance, isWall);
+        wallLeft = Physics.Raycast(transform.position, -orientation.right, out leftWallHit, wallCheckDistance, isWall);
+    }
+    private bool AboveGround()
+    {
+        return !Physics.Raycast(transform.position, Vector3.down, minJunpHeight, isGround);
+    }
+    private void StateMachine()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if ((wallLeft || wallRight) && verticalInput > 0 && AboveGround())
+        {
+            StartWallRun();
+        }
+        else 
+        {
+            EndWallRun();
+        }
+    }
+    private void StartWallRun()
+    {
+
+    }
+    private void WallRunning()
+    {
+        rb.useGravity = false;
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        Vector3 wallNormal = wallRight ? rightWallHit.normal : leftWallHit.normal;
+
+        Vector3 wallForward = Vector3.Cross(wallNormal, transform.up);
+
+        if ((orientation.forward - wallForward).magnitude > (orientation.forward - -wallForward).magnitude)
+            wallForward = -wallForward;
+
+        // forward force
+        rb.AddForce(wallForward * wallRunForce, ForceMode.Force);
+
+        // push to wall force
+        if (!(wallLeft && horizontalInput > 0) && !(wallRight && horizontalInput < 0))
+            rb.AddForce(-wallNormal * 100, ForceMode.Force);
+    }
+    private void EndWallRun()
+    {
+
     }
 }
