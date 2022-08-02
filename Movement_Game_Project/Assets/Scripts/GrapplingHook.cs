4799607155bc10e1032c62ab636964c0b746c774
Frankz-047 <75054025+Frankz-047@users.noreply.github.com>
2022-08-02 14:21:10 -0700

    added Simple cross hair

--- Movement_Game_Project/Assets/Scripts/GrapplingHook.cs
+++ Movement_Game_Project/Assets/Scripts/GrapplingHook.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GrapplingHook : MonoBehaviour
 {
     [SerializeField] private CharacterController _controller;
     [SerializeField] private Transform _grapplingHook;
