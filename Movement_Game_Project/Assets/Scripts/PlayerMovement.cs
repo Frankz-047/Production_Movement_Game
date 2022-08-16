0a478d5a48ab32d884149b76884e94ac38d1c173
Leohjz97 <79237457+Leohjz97@users.noreply.github.com>
2022-08-16 14:02:43 -0700

    lastest version

--- Movement_Game_Project/Assets/Scripts/PlayerMovement.cs
+++ Movement_Game_Project/Assets/Scripts/PlayerMovement.cs
@@ -5,31 +5,46 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    public float moveSpeed;
+    private float moveSpeed;
+    private float desiredMoveSpeed;
+    private float lastDesiredMoveSpeed;
+    
+    public float walkspeed;
+    public float wallrunSpeed;
     public float groundDrag;
+    
+    [Header("Jump")]
     public float jumpForce;
     public float jumpCooldown;
     public float airForce;
     bool CanJump;
-    int jumpCharge;
     public KeyCode jumpKey = KeyCode.Space;
 
     [Header("Ground")]
     public float playerHeight;
     public LayerMask Ground;
-    bool isGround;
-    bool checkGrounded;
+    public bool isGround;
 
     [Header("Setting")]
     public Transform orientation;
-    public KeyCode spawnPlatform = KeyCode.F;
-    public GameObject platform;
     float horizontalInput;
     float verticalInput;
     Vector3 moveDirection;
     Rigidbody rb;
 
-    public PlayerCam rotation;
+    public MovementState state;
+    public enum MovementState
+    {
+        restricted,
+        walking,
+        wallrunning,
+        air
+    }
+
+    //public bool sliding;
+    //public bool crouching;
+    public bool restricted; // no wasd movement
+    public bool wallrunning;
 
     private void Start()
     {
@@ -37,25 +52,15 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
 
         CanJump = true;
-        jumpCharge = 2;
     }
 
     private void Update()
     {
         isGround = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, Ground);
 
-        // better solutions welcome, set this way so both only check for one frame
-        if (!isGround && !checkGrounded)
-        {
-            checkGrounded = true;
-        }
-        if (isGround && checkGrounded)
-        {
-            ResetJump();
-        }
-
         MyInput();
         SpeedControl();
+        StateHandler();
 
         // handle drag
         if (isGround)
@@ -65,7 +70,8 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        MovePlayer();
+        if (state != MovementState.restricted)
+            MovePlayer();
     }
 
     private void MyInput()
@@ -73,41 +79,69 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKeyDown(jumpKey) && CanJump)
+        //To Jump
+        if (Input.GetKey(jumpKey) && CanJump && isGround)
         {
-            --jumpCharge;
-
-            if (!isGround)
-            {
-                --jumpCharge;
-            }
-
-            if (jumpCharge <= 0)
-            {
-                CanJump = false;
-            }
+            CanJump = false;
 
             Jump();
-        }
 
-        if (Input.GetKeyDown(spawnPlatform))
-        {
-            Vector3 position = gameObject.transform.position;
-            position.y -= (playerHeight * 0.55f);
-            Instantiate(platform, position, orientation.transform.rotation);
+            Invoke(nameof(ResetJump), jumpCooldown);
         }
     }
+
+    private void StateHandler()
+    {
+        // Mode - Restricted (no input)
+        if (restricted)
+        {
+            state = MovementState.restricted;
+        }
+
+        // Mode - Wallrunning
+        if (wallrunning)
+        {
+            state = MovementState.wallrunning;
+            desiredMoveSpeed = wallrunSpeed;
+        }
+
+        // Mode - Walking
+        else if (isGround)
+        {
+            state = MovementState.walking;
+            desiredMoveSpeed = walkspeed;
+        }
+        // Mode - Air
+        else
+        {
+            state = MovementState.air;
+        }
+        // check if desired move speed has changed drastically
+        if (Mathf.Abs(desiredMoveSpeed - lastDesiredMoveSpeed) > 4f && moveSpeed != 0)
+        {
+            StopAllCoroutines();
+
+            print("Lerp Started!");
+        }
+        else
+        {
+            moveSpeed = desiredMoveSpeed;
+        }
+
+        lastDesiredMoveSpeed = desiredMoveSpeed;
+    }
+
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
+        //on ground
         if (isGround)
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-
+            rb.AddForce(moveDirection.normalized * walkspeed * 10f, ForceMode.Force);
         // in air
         else if (!isGround)
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airForce, ForceMode.Force);
+            rb.AddForce(moveDirection.normalized * walkspeed * 10f * airForce, ForceMode.Force);
     }
 
     private void SpeedControl()
@@ -115,9 +149,9 @@ public class PlayerMovement : MonoBehaviour
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
         // limit velocity if needed
-        if (flatVel.magnitude > moveSpeed)
+        if (flatVel.magnitude > walkspeed)
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            Vector3 limitedVel = flatVel.normalized * walkspeed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
     }
@@ -129,16 +163,8 @@ public class PlayerMovement : MonoBehaviour
 
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
-
     private void ResetJump()
     {
-        checkGrounded = false;
-        jumpCharge = 2;
         CanJump = true;
-    }
-
-    public bool OnGround()
-    {
-        return isGround;
     }
 }
